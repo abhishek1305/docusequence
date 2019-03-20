2623034d81d23e6a3bed549e959c81e055713306
Abhishek <abhishek.sinha@technocube.in>
2019-03-20 17:03:50 +0530

    css-loader, url-loader, svh-url-loade added to the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -7,16 +7,45 @@ const htmlPlugin = new HtmlWebPackPlugin({
 
 
 module.exports = {
-    module: {
+    module: {      
       rules: [
         {
+          use: 'babel-loader',
           test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/, 
+          use : ['style-loader', 'css-loader'] 
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              fallback: 'responsive-loader',
+              quality: 85,
+              mimetype: 'image/png'
+            }
+          } ]
+        },
+          {
+            test: /\.(png|jpg|gif|svg|eot|woff|ttf)$/,
+            use: [
+            {
+              loader: 'file-loader'
+            }]
+          },
+          {
+            test: /\.svg/,
+            use: {
+            loader: 'svg-url-loader',
+            options: {}
+            }
           }
-        }
-      ]
+        ]
     },
     plugins: [htmlPlugin]
   };
